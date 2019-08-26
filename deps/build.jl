335ce87d41e25cdf39479b0a45ebf22393d18c22
Iblis Lin <iblis@hs.ntnu.edu.tw>
2019-08-26 11:09:08 +0800

    Upgrade to Julia v1.0 (#6)
    * wip
    
    * ignore
    
    * update build.jl
    
    * fix depwarns
    
    * update travis
    
    * Project.toml
    
    * update ArcadeLearningEnvironment
    
    * drop 0.7

--- deps/build.jl
+++ deps/build.jl
@@ -1,29 +1,28 @@
+romdir = joinpath(@__DIR__, "rom_files")
+rm(romdir, recursive=true, force=true)
+mkpath(romdir)
 
-# dir = dirname(@__FILE__)
-# romdir = joinpath(dir, "rom_files")
-# if !isdir(romdir)
-#     mkdir(romdir)
-#
-#     # download and unzip the roms, then cleanup zips
-#     urlbase = "http://www.atariage.com/2600/emulation/RomPacks/"
-#     for fn in ["Atari2600_A-E.zip",
-#                "Atari2600_F-J.zip",
-#                "Atari2600_K-P.zip",
-#                "Atari2600_Q-S.zip",
-#                "Atari2600_T-Z.zip"]
-#         localfn = joinpath(romdir, fn)
-#         download(urlbase*fn, localfn)
-#         run(`unzip -u $localfn -d $romdir`)
-#         rm(localfn)
-#     end
-#
-#     # rename all to lowercase letters
-#     for fn in readdir(romdir)
-#         newfn = lowercase(fn)
-#         if newfn != fn
-#             try
-#                 mv(joinpath(romdir, fn), joinpath(romdir, newfn))
-#             end
-#         end
-#     end
-# end
+# download and unzip the roms, then cleanup zips
+urlbase = "http://www.atariage.com/2600/emulation/RomPacks/"
+for fn in ["Atari2600_A-E.zip",
+           "Atari2600_F-J.zip",
+           "Atari2600_K-P.zip",
+           "Atari2600_Q-S.zip",
+           "Atari2600_T-Z.zip"]
+  localfn = joinpath(romdir, fn)
+  download(urlbase * fn, localfn)
+  run(`unzip -u $localfn -d $romdir`)
+  rm(localfn)
+end
+
+# rename all to lowercase letters
+for fn in readdir(romdir)
+  newfn = lowercase(fn)
+  (newfn == fn) && continue
+
+  try
+    mv(joinpath(romdir, fn), joinpath(romdir, newfn))
+  catch
+    @warn "rename $fn to lowercase failed"
+  end
+end
